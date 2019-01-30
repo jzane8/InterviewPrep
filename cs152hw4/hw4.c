0a40b1141d94493e536c2ba70858a0ab8c7ea8a8
Jake Zane <jzane16@kentdenver.org>
2019-01-30 17:45:12 -0600

    more pushes

--- cs152hw4/hw4.c
+++ cs152hw4/hw4.c
@@ -11,7 +11,7 @@ record* find_highest_record_by_date(const char* date, record*** rs,
   int num_days)
     {
       // first: convert date string to tm* struct
-      struct tm* convDate = malloc(sizeof(struct tm));
+      struct tm convDate = malloc(1+sizeof(struct tm));
       strptime(date, "%F", convDate);
       //second: find the record!
       for(int i = 0; i < num_days; ++i) {
